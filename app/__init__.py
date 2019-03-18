b6ba8fab01cb7cdf374b2e21d91b804f5c0df563
Kenneth Gajefski <kenneth.ag@outlook.com>
2019-03-18 13:20:43 -0400

    Simple SQL changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://john:pass1234@localhost/mydatabase'
 
 from app import routes
