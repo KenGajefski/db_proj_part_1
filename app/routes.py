cf64de6c53c322ceeb2a05c0c92d7b44c162901f
nickl95 <48530027+nickl95@users.noreply.github.com>
2019-04-18 19:09:23 -0400

    Added button
    Added the Insert/Delete/Update a paper button to the site.

--- app/routes.py
+++ app/routes.py
@@ -31,7 +31,67 @@ def main_page():
     # Using the same xxx.is_submitted() function for each and flashing the message will be best
     # Main will always be the rendered template
     cnx = mysql.connector.connect(user='john', password='pass1234')
+    cursor = cnx.cursor()from flask import Flask, render_template, flash, redirect, Session
+from app import app
+from app.login_form import LoginForm
+from app.main_forms import CreateDB, AssignReviewers, EditPapers
+from app.database import DBCreation
+import mysql.connector
+from mysql.connector import errorcode
+
+
+app.config['SECRET_KEY'] = 'the-penguin-is-a-most-serene-animal'
+# session is used for passing data between routes
+session = {}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    login_form = LoginForm()
+    # This is clearly not secure, but is a quick and nasty auth method for the purpose of this project
+    if login_form.is_submitted() and login_form.username.data == 'john' and login_form.password.data == 'pass1234':
+        session['user'] = login_form.username.data
+        session['pass'] = login_form.password.data
+        flash('Logging in as {}'.format(login_form.username.data))
+        return redirect('main_page')
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/main_page', methods=['GET', 'POST'])
+def main_page():
+    # For each button that will be on the GUI for the project, a form will have to be created
+    # i.e.: loginForm, createDB, addReviewers
+    # Using the same xxx.is_submitted() function for each and flashing the message will be best
+    # Main will always be the rendered template
+    cnx = mysql.connector.connect(user='john', password='pass1234')
     cursor = cnx.cursor()
+    createDB = CreateDB()
+    assignReviewers = AssignReviewers()
+    EditPaper = EditPapers() # Added edit paper button -Nick
+    # Need to figure out how to get this to work with session[]
+    # database initialization and creation functions contained here
+    if createDB.is_submitted():
+        dbInit = DBCreation()
+        dbInit.drop_database(cursor)
+    # Drops db if exists -Nick
+        dbInit.create_database(cursor, cnx)
+        dbInit.create_table(cursor)
+        dbInit.init_values(cursor, cnx)
+        flash('Database created and initialized')
+        return redirect('main_page')
+
+    return render_template('main.html', createForm=createDB, assignReviewForm=assignReviewers, editPaper=EditPaper)
+
+# This page will have fields in the form for adding a reviewer to a paper
+@app.route('/assign_reviewers', methods=['GET', 'POST'])
+def assign_reviewers_page():
+    assignReviewers = AssignReviewers()
+    return render_template('assign_reviewers.html', assignReviewForm=assignReviewers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
     createDB = CreateDB()
     assignReviewers = AssignReviewers()
     # Need to figure out how to get this to work with session[]
