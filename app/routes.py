a58ae284c13a11d9d60a00e2e86dde8047bfbcf1
nickl95 <48530027+nickl95@users.noreply.github.com>
2019-04-17 22:39:34 -0400

    Added drop database functionality
    Took me a while but I figured out the mysql connector syntax enough to implement functionality to automatically drop the database if it exists. That way, we don't have to manually drop the database via mySQLShell or Workbench.

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,6 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-# This is necessary for the app
 app.config['SECRET_KEY'] = 'the-penguin-is-a-most-serene-animal'
 # session is used for passing data between routes
 session = {}
@@ -17,7 +16,6 @@ session = {}
 def index():
     login_form = LoginForm()
     # This is clearly not secure, but is a quick and nasty auth method for the purpose of this project
-    # All this does is check that the input for the username and password are equal to the credentials for user 'john'
     if login_form.is_submitted() and login_form.username.data == 'john' and login_form.password.data == 'pass1234':
         session['user'] = login_form.username.data
         session['pass'] = login_form.password.data
@@ -28,9 +26,9 @@ def index():
 
 @app.route('/main_page', methods=['GET', 'POST'])
 def main_page():
-    # For each button that will be on the GUI for the project, a from will have to be created
+    # For each button that will be on the GUI for the project, a form will have to be created
     # i.e.: loginForm, createDB, addReviewers
-    # Using the same <form_name>.is_submitted() function for each and flashing the message will be best
+    # Using the same xxx.is_submitted() function for each and flashing the message will be best
     # Main will always be the rendered template
     cnx = mysql.connector.connect(user='john', password='pass1234')
     cursor = cnx.cursor()
@@ -38,19 +36,15 @@ def main_page():
     assignReviewers = AssignReviewers()
     # Need to figure out how to get this to work with session[]
     # database initialization and creation functions contained here
-
-    # This is how you differentiate which button is pressed on the page
-    if request.method == 'POST':
-        if "create-database" in request.form:
-            dbInit = DBCreation()
-            dbInit.create_database(cursor, cnx)
-            dbInit.create_table(cursor)
-            dbInit.init_values(cursor, cnx)
-            flash('Database created and initialized')
-            return redirect('main_page')
-        elif "assign-reviewers" in request.form:
-            flash('Assign button under construction')
-            return redirect('main_page')
+    if createDB.is_submitted():
+        dbInit = DBCreation()
+        dbInit.drop_database(cursor)
+    # Drops db if exists -Nick
+        dbInit.create_database(cursor, cnx)
+        dbInit.create_table(cursor)
+        dbInit.init_values(cursor, cnx)
+        flash('Database created and initialized')
+        return redirect('main_page')
 
     return render_template('main.html', createForm=createDB, assignReviewForm=assignReviewers)
 
