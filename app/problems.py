351cdc9f6e2c7d3438818851f4c437d11d1dc2cb
Kenneth Gajefski <kenneth.ag@outlook.com>
2019-04-21 22:22:22 -0400

    Minor fixes.

--- app/problems.py
+++ app/problems.py
@@ -1,23 +1,20 @@
 from flask import flash
 
 class Prob4:
-    def searchFotouhi(cursor, cnx):
+    def searchFotouhi(self, cursor, cnx):
         cursor.execute("USE sampleDB")
-
         query2 = "SELECT titlePaper FROM author, authorList A, paper P WHERE emailAuthor = 'fotouhi@t.com' " \
                  "AND email = 'fotouhi@t.com' AND A.paperID = P.paperID;"
-
         cursor.execute(query2)
         for (titlePaper) in cursor:
             flash("Fotouhi is author of {}".format(titlePaper))
-
         cursor.close()
         cnx.close()
         # This is how you clear session
         # session.pop('name', None)
 
 class Prob5:
-    def problem5(cursor, cnx):
+    def problem5(self, cursor, cnx):
         cursor.execute("USE sampleDB")
 
         for (titlePaper) in cursor:
